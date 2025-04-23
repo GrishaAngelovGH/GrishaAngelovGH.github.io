34bae9c9a65fb222a8ad07dc1819b2dc756b3d0f
Grisha Angelov <grisha.angelov89@gmail.com>
2025-04-23 09:12:07 +0300

    Extract the menu structure into separate Menu component

--- projects-overview/astro.config.mjs
+++ projects-overview/astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: "../docs"
+  outDir: "../docs",
+  vite: {
+    resolve: {
+      alias: {
+        '@components': path.resolve('./src/components')
+      }
+    }
+  }
 });
